d35c7e742b1517f4f6d96eb7e16cb00fecdcd12b
Bidit Mangal <biditmangal9506@gmail.com>
2021-02-25 21:54:00 +0530

    Add solution for problem `A - XXXXX` Link: https://codeforces.com/contest/1364/problem/A Tags: brute force, data structures, number theory, two pointers, *1200 Ref: https://codeforces.com/contest/1364/submission/108489240

--- codeforces/1364/A.cpp
+++ codeforces/1364/A.cpp
@@ -1,80 +1,30 @@
-#include <math.h>
-#include <time.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <array>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-#define sp system("pause")
-#define FOR(i, a, b) for (int i = a; i < b; ++i)
-#define FORD(i, a, b) for (int i = a; i > b; --i)
-#define REP(i, n) FOR(i, 0, (int)n - 1)
-#define pb(x) push_back(x)
-#define mp(a, b) make_pair(a, b)
-#define MS0(x) memset(x, 0, sizeof(x))
-#define MS1(x) memset(x, 1, sizeof(x))
-#define SORT(a, n) sort(begin(a), begin(a) + n)
-#define REV(a, n) reverse(begin(a), begin(a) + n)
-#define ll long long
-#define pii pair<int, int>
-#define MOD 1000000007
-
-void solve()
-{
-    // TODO.
-    int n, x;
-    cin >> n >> x;
-    int sum=0,l=-1,r;
-    int ans=0;
-    FOR(i, 0, n)
-    {
-
-        int c;
-        cin >> c;
-        if(c%x){
-            if(l==-1){
-                l=i;
-            }
-            r = i;
-        }
-        sum+=c;
-    }
-    if(sum%x){
-        ans=n;
-    }
-    else if(l==-1){
-        ans=-1;
-    }
-    else{
-        ans=n-min(l+1,n-r);
-    }
-    cout<<ans<<endl;
-}
 int main()
 {
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t = 1;
-    cin >> t;
+    int t;
+    scanf("%d",&t);
     while (t--)
-        solve();
-
-    return 0;
+    {
+        int n,x,sum=0,l=-1,r;
+        scanf("%d%d",&n,&x);
+        for (int i=0;i<n;i++)
+        {
+            int a;
+            scanf("%d",&a);
+            if (a%x)
+            {
+                if (l==-1)
+                l=i;
+                r=i;
+            }
+            sum+=a;
+        }
+        if (sum%x)
+        printf("%d\n",n);
+        else if (l==-1)
+        printf("-1\n");
+        else
+        printf("%d\n",n-min(l+1,n-r));
+    }
 }
